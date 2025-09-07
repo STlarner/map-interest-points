cfdd0e2e3d974d36c8c9432bdb719d9cd2851c45
Lorenzo D'Alessandro <lorenzodalessandro@Host-002.homenet.telecomitalia.it>
2025-09-07 18:34:58 +0200

    main: reworked homepage with sliver, added loading state in trip details

--- packages/app/lib/ui/screens/home_screen/home_screen.dart
+++ packages/app/lib/ui/screens/home_screen/home_screen.dart
@@ -27,82 +27,103 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              GetIt.I<SessionManager>().logout();
-              context.goNamed(AppRoute.login.name);
-            },
-            icon: const Icon(Icons.person),
-          ),
-        ],
-      ),
-      body: RefreshIndicator(
-        onRefresh: () =>
-            context.read<TripsNotifier>().fetchAllUserTrips(forceRefresh: true),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
-          child: ListView(
-            clipBehavior: Clip.none,
-            children: [
-              Text("Upcoming Trips", style: context.textTheme.titleLarge),
-              const SizedBox(height: 16),
-              Consumer<TripsNotifier>(
-                builder: (context, tripsNotifier, child) {
-                  switch (tripsNotifier.upcomingTripsState.status) {
-                    case AsyncStatus.initial:
-                    case AsyncStatus.loading:
-                      return ListView.separated(
-                        separatorBuilder: (context, index) =>
-                            const Padding(padding: EdgeInsets.only(bottom: 24)),
-                        itemCount: 2,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return Shimmer.fromColors(
-                            baseColor: Colors.grey.shade300,
-                            highlightColor: Colors.grey.shade100,
-                            period: const Duration(milliseconds: 1000),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                                color: context.colorScheme.surface,
-                              ),
-                              height: 130,
-                            ),
-                          );
-                        },
-                      );
-                    case AsyncStatus.error:
-                      return const Center(child: CircularProgressIndicator());
-                    case AsyncStatus.success:
-                      return ListView.builder(
-                        itemCount:
-                            tripsNotifier.upcomingTripsState.data!.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          final trip =
-                              tripsNotifier.upcomingTripsState.data![index];
-                          return TripCardWidget.fromTripModel(
-                            tripModel: trip,
-                            onTap: () => context.pushNamed(
-                              AppRoute.tripDetail.name,
-                              pathParameters: {"tripId": trip.id},
-                            ),
-                          );
-                        },
-                      );
-                  }
+    return Consumer<TripsNotifier>(
+      builder: (context, tripsNotifier, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Home"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  GetIt.I<SessionManager>().logout();
+                  context.goNamed(AppRoute.login.name);
                 },
+                icon: const Icon(Icons.person),
               ),
             ],
           ),
-        ),
-      ),
+          body: RefreshIndicator(
+            onRefresh: () => context.read<TripsNotifier>().fetchAllUserTrips(
+              forceRefresh: true,
+            ),
+            child: CustomScrollView(
+              slivers: [
+                SliverPadding(
+                  padding: const EdgeInsets.only(
+                    top: 24,
+                    left: 24,
+                    right: 24,
+                    bottom: 16,
+                  ),
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate([
+                      Text(
+                        "Upcoming Trips",
+                        style: context.textTheme.titleLarge,
+                      ),
+                    ]),
+                  ),
+                ),
+
+                if (tripsNotifier.upcomingTripsState.status ==
+                    AsyncStatus.loading)
+                  SliverPadding(
+                    padding: const EdgeInsets.only(
+                      left: 24.0,
+                      right: 24.0,
+                      bottom: 100,
+                    ),
+                    sliver: SliverList.separated(
+                      separatorBuilder: (context, index) =>
+                          const Padding(padding: EdgeInsets.only(bottom: 24)),
+                      itemBuilder: (context, index) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.grey.shade300,
+                          highlightColor: Colors.grey.shade100,
+                          period: const Duration(milliseconds: 1000),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: context.colorScheme.surface,
+                            ),
+                            height: 130,
+                          ),
+                        );
+                      },
+                      itemCount: 2,
+                    ),
+                  ),
+
+                if (tripsNotifier.upcomingTripsState.status ==
+                    AsyncStatus.success)
+                  SliverPadding(
+                    padding: const EdgeInsets.only(
+                      left: 24.0,
+                      right: 24.0,
+                      bottom: 120,
+                    ),
+                    sliver: SliverList.separated(
+                      separatorBuilder: (context, index) =>
+                          const Padding(padding: EdgeInsets.only(bottom: 24)),
+                      itemBuilder: (context, index) {
+                        final trip =
+                            tripsNotifier.upcomingTripsState.data![index];
+                        return TripCardWidget.fromTripModel(
+                          tripModel: trip,
+                          onTap: () => context.pushNamed(
+                            AppRoute.tripDetail.name,
+                            pathParameters: {"tripId": trip.id},
+                          ),
+                        );
+                      },
+                      itemCount: tripsNotifier.upcomingTripsState.data!.length,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- packages/app/lib/ui/screens/trip_detail_screen/trip_detail_screen.dart
+++ packages/app/lib/ui/screens/trip_detail_screen/trip_detail_screen.dart
@@ -3,6 +3,7 @@ import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 import "package:ui/ui.dart";
 
+import "../../../notifiers/async_state.dart";
 import "../../../notifiers/trip_detail_notifier.dart";
 import "../../widgets/firebase_async_image.dart";
 import "../../widgets/trip_day_card.dart";
@@ -94,27 +95,56 @@ class _TripDetailScreenState extends State<TripDetailScreen> {
 
               const SliverToBoxAdapter(child: SizedBox(height: 32)),
 
-              SliverPadding(
-                padding: const EdgeInsets.only(
-                  left: 24.0,
-                  right: 24.0,
-                  bottom: 100,
+              if (tripNotifier.interestPointsStatus == AsyncStatus.loading)
+                SliverPadding(
+                  padding: const EdgeInsets.only(
+                    left: 24.0,
+                    right: 24.0,
+                    bottom: 100,
+                  ),
+                  sliver: SliverList.separated(
+                    separatorBuilder: (context, index) =>
+                        const Padding(padding: EdgeInsets.only(bottom: 24)),
+                    itemBuilder: (context, index) {
+                      return Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        period: const Duration(milliseconds: 1000),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: context.colorScheme.surface,
+                          ),
+                          height: 130,
+                        ),
+                      );
+                    },
+                    itemCount: 3,
+                  ),
                 ),
-                sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate((context, index) {
-                    final entry = tripNotifier.interestPointsByDay.entries
-                        .elementAt(index);
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 24.0),
-                      child: TripDayCard(
+
+              if (tripNotifier.interestPointsStatus == AsyncStatus.success)
+                SliverPadding(
+                  padding: const EdgeInsets.only(
+                    left: 24.0,
+                    right: 24.0,
+                    bottom: 120,
+                  ),
+                  sliver: SliverList.separated(
+                    separatorBuilder: (context, index) =>
+                        const Padding(padding: EdgeInsets.only(bottom: 24)),
+                    itemBuilder: (context, index) {
+                      final entry = tripNotifier.interestPointsByDay.entries
+                          .elementAt(index);
+                      return TripDayCard(
                         interestPoints: entry.value,
                         day: index + 1,
                         date: entry.key,
-                      ),
-                    );
-                  }, childCount: tripNotifier.interestPointsByDay.length),
+                      );
+                    },
+                    itemCount: tripNotifier.interestPointsByDay.length,
+                  ),
                 ),
-              ),
             ],
           ),
         );
